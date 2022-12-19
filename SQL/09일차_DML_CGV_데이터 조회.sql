34f5c6d8ff079547473d2ad35115105e98d3e3fe
HyunKyuLee <fosa152@gmail.com>
2022-12-19 21:37:58 +0900

    09일차 : CGV 조회 예제

--- SQL/09일차_DML_CGV_데이터 조회.sql
+++ SQL/09일차_DML_CGV_데이터 조회.sql
@@ -33,3 +33,75 @@ select se_name as 'abc회원이 예매한 영화 올빼미 좌석' from ticketin
     join ticketing_seat on ts_ti_num = ti_num
     join seat on ts_se_num = se_num
     where mo_title like '올빼미' and ti_me_id like 'abc';
+    
+-- 올빼미 12월 20일 13:50 상영에 예매 가능한 좌석 조회
+select ss_possible_seat as '예매 가능한 올빼미 좌석' from seat
+	join ticketing_seat on ts_se_num = se_num
+    join cinema on se_ci_num = ci_num
+    join screen_schedule  on ss_ci_num = ci_num
+    join movie on mo_num = ss_mo_num
+    join ticketing on ti_num = ts_ti_num
+    where mo_title like '올빼미';
+-- screen_schedule, movie, ticketing, ticketing_seat, seat    
+select mo_title as 영화제목, se_name as 좌석명,
+	case
+		when ts_num is null
+        then 'O'
+        else 'X'
+	end as 가능여부
+ from
+	(select * from screen_schedule where ss_date = '2022-12-20' and ss_time = '13:50' ) as ss
+	join (select * from movie where mo_title like '올빼미') as mo 
+		on mo_num = ss_mo_num
+	join cinema 
+		on ss_ci_num = ci_num
+	join seat
+		on se_ci_num = ci_num
+	left join ticketing
+		on ti_ss_num = ss_num
+	left join ticketing_seat
+		on ts_se_num = se_num;
+        
+-- screen_schedule에 영화 제목 속성을 추가하면 movie 테이블과 join을 하지 않아도 됨 단, 데이터는 중복
+-- ticketing_seat에 좌석 이름을 추가하면 좌석명을 알기 위해 seat테이블과 join하지 않아도 됨.
+
+-- 영화별 누적 관객 수를 조회
+select mo_title as 영화제목,ti_amount as '누적관객수' from ticketing
+join screen_schedule on ss_num = ti_ss_num
+join movie on mo_num = ss_mo_num;
+
+select mo_title as 영화제목,ifnull(sum(ti_amount),0) as '누적관객수' from movie
+	left join (select * from screen_schedule where ss_date <= now()) as ss
+		on ss_mo_num = mo_num
+    left join ticketing on ti_ss_num = ss_num
+    group by mo_num;
+
+-- 영화 순위( 누적별 관객 수)를 조회
+select mo_title as 영화제목,ifnull(sum(ti_amount),0) as '누적관객수' from movie
+	left join (select * from screen_schedule where ss_date <= now()) as ss
+		on ss_mo_num = mo_num
+    left join ticketing on ti_ss_num = ss_num
+    group by mo_num
+    order by 관객수 desc;
+    
+-- 날짜가 지났을 때 변경되어야 할 쿼리를 작성
+-- 회원이 본 영화 체크
+
+UPDATE member 
+	SET 
+		me_movie_count = (select 
+				count(distinct ss_mo_num)
+            from
+				ticketing
+					join
+				screen_schedule on ss_num = ti_ss_num
+			where
+				ss_date < now())
+	WHERE 
+		me_id = 'abc';
+    
+-- 'abc' 회원이 본 영화 목록 개수
+select count(distinct ss_mo_num) from ticketing
+	join screen_schedule on ss_num = ti_ss_num
+    where ss_date <= now();
+
